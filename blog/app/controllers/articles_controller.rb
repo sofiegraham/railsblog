cf9ef8b567f2811e92a13483a7a66523bf7a17dc
sofiegraham <sofiegraham@hotmail.com>
2017-01-14 10:32:49 -0800

    Added commenting ability...and more chickens

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "sofie", password: "password", except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
   
   def index
     @articles = Article.all
@@ -35,6 +41,14 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
   end
 
 
